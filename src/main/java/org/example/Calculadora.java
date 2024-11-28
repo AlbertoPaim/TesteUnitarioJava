75cd31e914c1b4f1eba87a51c9513aa4a4ef8241
Alberto Paim <albertoopaim@gmail.com>
2024-11-28 09:53:34 -0300

    Teste Unitário

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calculadora {
+    public double somar(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public double subtrair(double num1, double num2){
+        return num1 - num2;
+    }
+
+}
---
+++ src/test/java/org/example/CalculadoraTest.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CalculadoraTest {
+    private Calculadora calculadora;
+
+    @Before
+    public void setup() {
+        calculadora = new Calculadora();
+    }
+
+    @Test
+    public void testarSe2mais2e4() {
+        double a = 2;
+        double b = 2;
+        double resultadoEsperado = 4;
+
+        double resultadoObtido = calculadora.somar(a, b);
+
+        Assert.assertEquals(resultadoEsperado, resultadoObtido, 000.1);
+    }
+
+    @Test
+    public void testarSe2Menos2e0() {
+        double a = 2;
+        double b = 2;
+        double resultadoEsperado = 0;
+        double resultadoObtido = calculadora.subtrair(a, b);
+        Assert.assertEquals(resultadoEsperado, resultadoObtido, 00.1);
+    }
+}
